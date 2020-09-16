c9c37c6999a453deb34e443cb49436362c733cc4
koronan625 <koronan625.gmail.com>
2020-09-16 22:41:00 +0900

    formを2個に変更

--- final/tango.py
+++
@@ -1,9 +0,0 @@
-from flask import flask
-
-app = Flask(__name__, static_folder='.', static_url_path='')
-
-@app.route('/')
-def index():
-    return app.send_static_file('result.html')
-
-app.run(port=8000, debug=True)
